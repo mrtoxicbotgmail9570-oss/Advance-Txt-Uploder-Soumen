86576d0fbb8fdb0015d588576883578659ef19d7
kajla4053-hash <kajla4053@gmail.com>
2025-12-08 14:12:10 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,11 +3,11 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "13076045"))
-API_HASH = environ.get("API_HASH", "32f2036dcb66c94637a7e87de6b6001b")
+API_ID = int(environ.get("API_ID", "22480303"))
+API_HASH = environ.get("API_HASH", "99c931b6c1ae6f8c3c3e87da173fa424")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "8222519141"))
+OWNER = int(environ.get("OWNER", "7518770522"))
 CREDIT = environ.get("CREDIT", "𝘽𝙊𝙏𝙎")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
